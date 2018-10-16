116212325b12ce532997a312b3aad5ecc32554e2
Kanduleek <34539376+Kanduleek@users.noreply.github.com>
2018-10-16 13:07:22 +0200

    Add files via upload

---
+++ STATA/Zajecia 1.R
@@ -0,0 +1,16 @@
+#Zadanie 1 
+x<-c(rep(TRUE, each = 3),rep(FALSE, each = 4),rep(FALSE, each = 4),rep(FALSE, each = 5))
+x
+
+#Zadanie 2
+b<-1/(seq(0, 1000, by=2))
+b
+
+#Zadanie 3
+a<-c(0:19,rep(0, each = 10),seq(0, 40, by=2),seq(40, 0, by=-2),rep(0,each = 10),19:0)
+a
+
+#Zadanie 4
+
+
+
